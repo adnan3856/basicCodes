9fc431094fcf4c2990a9f90b4e724ece39a92186
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2020-09-25 11:45:35 +0530

    Create PowerUsingRecursion.java

---
+++ Power of a number/PowerUsingRecursion.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PowerUsingRecursion {
+
+    public static int power(int n, int p) {
+        int result=0;
+        if(p==0)
+            result=1;
+        else{
+            result= n*power(n,p-1);
+        }
+    return result;
+
+    }
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int num = scan.nextInt();
+        System.out.print("Enter the power:");
+        int pow = scan.nextInt();
+        int result = power(num, pow);
+        System.out.println(result);
+    }
+}
