2a2d4f05076c02880750bbbe7ed461b7eac679af
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2020-09-25 11:25:02 +0530

    Create usingRecursion.java

---
+++ 1 to 100 without loops/usingRecursion.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class LowerToUpper{
+    static int i=1;
+    public static void main(String args[]){
+
+       if(i<=100){
+           System.out.println(i);
+               i++;
+            main(null);
+       }
+    }
+}
