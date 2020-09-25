0943b510b7501c092088b205a0519d82a26b88fb
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2020-09-25 11:08:55 +0530

    Create lowerToUpper.java

---
+++ Lower to Upper Case/lowerToUpper.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class LowerToUpper {
+    public static void main(String args[]){
+        Scanner scan= new Scanner((System.in));
+        System.out.println("Enter the String in CAPITAL:");
+        String s=scan.nextLine();
+        System.out.println(s.toLowerCase());
+
+    }
+}
