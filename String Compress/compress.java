d0a03fd9bacf3c8dd1b0368caee2cc875e59d435
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2020-09-27 11:10:46 +0530

    Create compress.java

---
+++ String Compress/compress.java
@@ -0,0 +1,27 @@
+public class compress {
+
+    public static void main(String[] args) {
+        String s="";
+        String s1="a";
+        int n=s1.length();
+        char c=s1.charAt(0);
+        int count=1;
+        for(int i=1;i<n;i++)
+        {
+            if(c==s1.charAt(i)){
+                count++;
+            }
+            else{
+                s+=c;
+                if(count>1)
+                    s+=count;
+                c=s1.charAt(i);
+                count=1;
+            }
+        }
+        s+=c;
+        if(count>1)
+            s+=count;
+        System.out.println(s); }
+}
+
