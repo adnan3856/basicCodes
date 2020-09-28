e00101d71bd5b2b417931e81eefda508554578b8
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2020-09-28 13:03:12 +0530

    Create solution.java

---
+++ Generate Multiplication Table/solution.java
@@ -0,0 +1,13 @@
+
+import java.io.*;
+import java.util.*;
+public class solution {
+
+    public static void main(String[] args) {
+
+      Scanner scan=new Scanner(System.in);
+      int n=scan.nextInt();
+      for(int i=1;i<=10;i++)
+      System.out.println(n + "X" +i + "=" + n*i);
+    }
+}
