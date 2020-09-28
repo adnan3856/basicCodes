abfdc5c949206b1304648abc54ddbc1feb6cec62
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2020-09-28 13:08:45 +0530

    Create solution2.java

---
+++ Swap two numbers/solution2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class solution2 {
+
+    public static void main(String[] args) {
+
+      Scanner scan=new Scanner(System.in);
+      int a=10,b=20;
+      a=a+b;
+      b=a-b;
+      a=a-b;
+      System.out.println("a="+a +","+"b="+ b);
+    }
+}
