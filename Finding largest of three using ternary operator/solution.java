1d914689c2a606f92804af5c9ab69641c199be7f
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2020-09-28 13:22:53 +0530

    Create solution.java

---
+++ Finding largest of three using ternary operator/solution.java
@@ -0,0 +1,8 @@
+public class solution {
+
+    public static void main(String[] args) {
+      int a=10,b=40,c=20;
+       int d= c>(a>b?a:b) ? c : (a>b?a:b);
+       System.out.println(d);
+    }
+}
